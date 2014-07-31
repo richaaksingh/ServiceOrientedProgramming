62a258d17b6f88adf891aa85c91a2bb62d5f0965
Binit <binit_shah2000@yahoo.com>
2014-07-30 20:46:55 -0400

    Initial build
    web services

---
+++ WebServices/ESPN/src/java/service/ESPNService.java
@@ -0,0 +1,137 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package service;
+
+import java.io.IOException;
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+/**
+ *
+ * @author Binit
+ */
+@WebService(serviceName = "ESPNService")
+public class ESPNService {
+
+    /**
+     * This is a sample web service operation
+     */
+    @WebMethod(operationName = "getSportsNews")
+    public String getSportsNews(@WebParam(name = "sportName") String sportName) throws SAXException, IOException, ParserConfigurationException {
+
+        int limit = 10;
+
+        if(sportName == null)
+            return null;
+        
+        String url = "http://api.espn.com/v1/sports/" + sportName;
+        String key = "?limit=" + limit + "&apikey=4d8h88kmrqjjv7xcxpwkdsh3&_accept=text/xml";
+
+        StringBuilder res = new StringBuilder();
+
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+        Document doc = dBuilder.parse(url + key);
+
+        doc.getDocumentElement().normalize();
+
+        NodeList currentList = doc.getDocumentElement().getChildNodes();
+
+        Node sports = currentList.item(0);
+
+        NodeList sportsItemList = sports.getChildNodes();
+
+        for (int i = 0; i < sportsItemList.getLength(); i++) {
+
+            Node sportsItem = sportsItemList.item(i);
+            NodeList sport = sportsItem.getChildNodes();
+
+            Node name = sport.item(0);
+            res.append(name.getTextContent());
+            res.append(";;");
+
+            Node links = sport.item(3);
+            if (links != null) {
+                Node api = links.getChildNodes().item(0);
+                if (api != null) {
+                    NodeList apiList = api.getChildNodes();
+                    for (int j = 0; j < apiList.getLength(); j++) {
+                        if ("headlines".equalsIgnoreCase(apiList.item(j).getNodeName())) {
+                            Node news = apiList.item(j);
+                            Node newsLink = news.getChildNodes().item(0);
+//                            System.out.println("Link: " + newsLink.getTextContent() + key);
+                            res.append(getNewsLink(newsLink.getTextContent() + key));
+                            break;
+                        }
+                    }
+                    res.append(";;");
+                }
+            }
+            
+            Node leagues = sport.item(4);
+            if (leagues != null) {
+                NodeList leaguesItemList = leagues.getChildNodes();
+
+                for (int j = 0; j < leaguesItemList.getLength(); j++) {
+                    Node leaguesItem = leaguesItemList.item(j);
+                    Node league = leaguesItem.getChildNodes().item(0);
+                    res.append(league.getTextContent());
+                    res.append("#");
+                }
+                res = new StringBuilder(res.substring(0, res.length() - 1));
+            } else {
+                res = new StringBuilder(res.substring(0, res.length() - 1));
+            }
+            res.append("~");
+        }
+        res = new StringBuilder(res.substring(0, res.length() - 1));
+        if (res.length() == 0) {
+            return null;
+        }
+        return res.toString();
+    }
+
+    public static String getNewsLink(String url) throws ParserConfigurationException, SAXException, IOException {
+
+        StringBuilder res = new StringBuilder();
+
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+        Document doc = dBuilder.parse(url);
+
+        doc.getDocumentElement().normalize();
+
+        NodeList currentList = doc.getDocumentElement().getChildNodes();
+
+        NodeList headLinesItemList = currentList.item(5).getChildNodes();
+
+        for (int i = 0; i < headLinesItemList.getLength(); i++) {
+            NodeList headLinesItem = headLinesItemList.item(i).getChildNodes();
+            res.append(headLinesItem.item(0).getTextContent());
+            res.append("@");
+//			System.out.println("headlines: " + headLinesItem.item(0).getTextContent());
+            for (int j = 0; j < headLinesItem.getLength(); j++) {
+                if ("links".equalsIgnoreCase(headLinesItem.item(j).getNodeName())) {
+                    res.append(headLinesItem.item(j).getChildNodes().item(1).getChildNodes().item(0).getTextContent());
+                    break;
+                }
+            }
+            res.append("#");
+//			System.out.println("link: " + headLinesItem.item(5).getChildNodes().item(1).getChildNodes().item(0).getTextContent());
+        }
+        res = new StringBuilder(res.substring(0, res.length() - 1));
+        return res.toString();
+
+    }
+}
---
+++ WebServices/Indeed/src/java/service/IndeedService.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package service;
+
+import java.io.IOException;
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+/**
+ *
+ * @author Binit
+ */
+@WebService(serviceName = "IndeedService")
+public class IndeedService {
+
+    /**
+     * This is a sample web service operation
+     */
+    @WebMethod(operationName = "searchJobs")
+    public String searchJobs(@WebParam(name = "name") String query, String location, String city, String jobTitle)
+            throws ParserConfigurationException, SAXException, IOException {
+
+        int resultNode = 19;
+
+        if (query == null) {
+            query = "";
+        }
+        location = (location == null) ? "" : location;
+        if ("".equals(city)) {
+            resultNode = resultNode - 2;
+        }
+        jobTitle = (jobTitle == null) ? "" : jobTitle;
+
+        StringBuilder res = new StringBuilder();
+        String url = "http://api.indeed.com/ads/apisearch?publisher=3715321615320257"
+                + "&q=" + query + "&l=" + location + "%2C+" + city + "&sort=&radius=&st=&jt=" + jobTitle + "&start=&limit=25&fromage=&"
+                + "filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&v=2";
+        try {
+
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(url);
+            doc.getDocumentElement().normalize();
+
+            NodeList currentList = doc.getDocumentElement().getChildNodes();
+
+            Node newNode = currentList.item(resultNode);
+            if (newNode != null) {
+                NodeList results = newNode.getChildNodes();
+
+                for (int i = 1; i < results.getLength(); i = i + 2) {
+                    Node result = results.item(i);
+                    NodeList searchResult = result.getChildNodes();
+
+                    Node title = searchResult.item(1);
+                    Node company = searchResult.item(3);
+                    Node loc = searchResult.item(11);
+                    Node date = searchResult.item(15);
+                    Node link = searchResult.item(19);
+
+                    res.append(title.getTextContent());
+                    res.append(";;");
+                    res.append(company.getTextContent());
+                    res.append(";;");
+                    res.append(loc.getTextContent());
+                    res.append(";;");
+                    res.append(date.getTextContent());
+                    res.append(";;");
+                    res.append(link.getTextContent());
+
+                    res.append("~");
+                }
+            }
+            res = new StringBuilder(res.substring(0, res.length() - 1));
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    //    res = new StringBuilder(url);
+        if (res.length() == 0) {
+            return null;
+        }
+        return res.toString();
+    }
+}
